de09036e46e00449f9f3bb6b0dbb9ff441082bb2
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-08 14:48:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+
+def introduction_with_language(phrase)
+  puts "Hi, my name is #{name} and I am learning to program in Ember.js"
+end
