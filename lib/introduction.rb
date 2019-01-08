7ac9a49141f17141040c9dc24b887269fc0a9bc4
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-08 14:59:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,3 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-
-def introduction_with_language(name)
-  puts "Hi, my name is #{name} and I am learning to program in Ember."
-end
--- lib/introduction_with_language.rb
+++ lib/introduction_with_language.rb
@@ -1,5 +1,5 @@
 # lib/introduction_with_language
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
